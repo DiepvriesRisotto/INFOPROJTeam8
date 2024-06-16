c82c244cb9d5408efca41119a8ac71cff0e1bc44
Robert Karzijn <Robertkarzijn@gmail.com>
2024-06-16 14:23:22 +0200

    V5.2

--- forum/forum.js
+++ forum/forum.js
@@ -1,17 +1,21 @@
     window.onload = function() {
-        var submitButton = document.querySelector('.buttons a[href="forumhome2.html"] button');
         var likeButton = document.getElementById('likeButton');
         var saveButton = document.getElementById('saveButton');
-    
-        submitButton.onclick = function() {
-            alert('Your new thread has been saved.');
+        var submitButton = document.getElementById('submitButton');
+
+        function showAlert(message) {
+            alert(message);
         }
-    
+
         likeButton.onclick = function() {
-            alert('Your like has been saved.');
+            showAlert('Like has been saved.');
         }
-    
+
         saveButton.onclick = function() {
-            alert('Thread has been saved.');
+            showAlert('Thread has been saved.');
+        }
+
+        submitButton.onclick = function() {
+            showAlert('Your new thread has been saved.');
         }
     }
